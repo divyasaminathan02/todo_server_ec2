05dbc28bd16c9e99cb383dbcf07b71bfe3f02c38
divyasaminathan <sridivyadharshini2007@gmail.com>
2025-12-17 10:07:09 +0530

    first commit

---
+++ Controller/todoController.js
@@ -0,0 +1,38 @@
+import todoCollection from "../Model/todoModel.js";
+
+export const addTodo= async (req, res) => {
+    try {
+        const data = new todoCollection(req.body);
+        await data.save();
+        res.status(200).json({ mess: "data has been started" })
+
+    } catch (err) {
+        res.status(400).json({ message: err })
+    }
+}
+export const getTodo= async (req, res) => {
+    try {
+        const data = await todoCollection.find();
+        res.json(data)
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+
+}
+export const updateTodo= async () => {
+    try {
+        const data = await todoCollection.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.json(data)
+    } catch (err) {
+        res.status(400).json({ message: err })
+
+    }
+}
+export const deleteTodo= async (req, res) => {
+    try {
+        const de = await todoCollection.findByIdAndDelete(req.params.id)
+        res.json({ message: "todo has been deleted" })
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+}
